69381acbe2bc706621205962f8af823fd7ce6af1
Jaeha Hwang <wogk216@naver.com>
2022-11-07 17:07:16 +0900

    Nil 합병 연산자

--- swift_practice05.playground/Contents.swift
+++ swift_practice05.playground/Contents.swift
@@ -18,4 +18,3 @@ nowColor = userpickColor ?? defaultColor
 print("현재 선택되어 있는 색은 \(nowColor)입니다") // 현재 선택되어 있는 색은 소라색입니다
 // userpickColor에 "소라색"값을 넣어줬기 때문에, nowColor에 ?? 앞에 값인 userpickColor의 "소라색"이 들어감
 // 그리고 언래핑되어 출력됨
-
